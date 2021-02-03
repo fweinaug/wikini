5eeb46f58e814386762537275eb4fd36ecd56890
Florian Weinaug <florianweinaug@outlook.de>
2021-02-03 21:16:45 +0100

    add geolocation service

---
+++ src/WikipediaApp/WikipediaApp/Model/Coordinates.cs
@@ -0,0 +1,14 @@
+﻿namespace WikipediaApp
+{
+  public class Coordinates
+  {
+    public double Latitude { get; }
+    public double Longitude { get; }
+
+    public Coordinates(double latitude, double longitude)
+    {
+      Latitude = latitude;
+      Longitude = longitude;
+    }
+  }
+}
---
+++ src/WikipediaApp/WikipediaApp/Services/GeolocationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace WikipediaApp
+{
+  public class GeolocationService
+  {
+    public async Task<Coordinates> GetCurrentLocation()
+    {
+      var accessStatus = await Geolocator.RequestAccessAsync();
+      if (accessStatus != GeolocationAccessStatus.Allowed)
+        return null;
+
+      var geolocator = new Geolocator();
+      var geoposition = await geolocator.GetGeopositionAsync();
+
+      return new Coordinates(geoposition.Coordinate.Point.Position.Latitude, geoposition.Coordinate.Point.Position.Longitude);
+    }
+  }
+}
