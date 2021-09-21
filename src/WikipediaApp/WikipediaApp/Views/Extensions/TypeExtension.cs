1a491fb038eb30667f839d1e199907882ed1abe4
Florian Weinaug <florianweinaug@outlook.de>
2021-09-21 19:45:10 +0200

    fix type

---
+++ src/WikipediaApp/WikipediaApp/Views/Extensions/TypeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Windows.UI.Xaml.Markup;
+
+namespace WikipediaApp
+{
+  [MarkupExtensionReturnType(ReturnType = typeof(Type))]
+  public sealed class TypeExtension : MarkupExtension
+  {
+    public string TypeName { get; set; }
+
+    protected override object ProvideValue()
+    {
+      return Type.GetType(TypeName);
+    }
+  }
+}
