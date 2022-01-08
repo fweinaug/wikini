90d793584e8b0f013033295ae153cf30bfef0816
Florian Weinaug <florianweinaug@outlook.de>
2022-01-08 09:44:35 +0100

    add localized language name to tooltip

--- src/WikipediaApp/WikipediaApp/ViewModels/ArticleLanguageViewModel.cs
+++ src/WikipediaApp/WikipediaApp/ViewModels/ArticleLanguageViewModel.cs
@@ -2,10 +2,12 @@
 {
   public class ArticleLanguageViewModel : LanguageViewModel
   {
+    public string LocalizedName { get; private set; }
     public string Title { get; private set; }
 
     public ArticleLanguageViewModel(ArticleLanguage language, int index) : base(language, index)
     {
+      LocalizedName = language.LocalizedName;
       Title = language.Title;
     }
   }
